9c3c5cc6af596122239d653caf337668389482c5
LilJdh1 <100974428+LilJdh1@users.noreply.github.com>
2024-10-01 10:34:56 -0500

    creacion de la conexion con firebase

---
+++ gestionReservas/src/firebase/firebaseConfig.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyD0vhB5aU3JyErJDZjhSoHAD7V-HlP8VWc",
+  authDomain: "gestionreserva-bf295.firebaseapp.com",
+  projectId: "gestionreserva-bf295",
+  storageBucket: "gestionreserva-bf295.appspot.com",
+  messagingSenderId: "1017798051082",
+  appId: "1:1017798051082:web:0535f92553c9531e08a6f3"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
