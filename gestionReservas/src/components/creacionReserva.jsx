cf9789b3c934a91ffa82b9d9cd4f00709d6801d6
LilJdh1 <100974428+LilJdh1@users.noreply.github.com>
2024-10-01 11:10:15 -0500

    se crea el componente de crear reserva

--- gestionReservas/src/components/creacionReserva.jsx
+++ gestionReservas/src/components/creacionReserva.jsx
@@ -1,10 +1,11 @@
 import React, {useState, useEffect} from "react";
 import {db} from "../firebase/firebaseConfig"
+import {collection, getDocs, addDoc, deleteDoc,updateDoc,doc} from 'firebase/firestore'
 
 const CreacionReserva =()=>{
     const[nombre, setNombre] = useState('');
     const[numeroDePersonas,setNumeroDePersonas] = useState(0);
-    const[fecha, setFecta] = useState('');
+    const[fecha, setFecha] = useState('');
     const[hora, setHora] = useState('');
     const[estado, setEstado] = useState('confirmada')
 
@@ -18,9 +19,31 @@ const CreacionReserva =()=>{
                 fecha,
                 hora,
                 estado
-            })
+            });
+            alert('Reserva creada con exito');
         } catch (error) {
-            
+            console.error(error);
+            alert('Error al crear la reserva')
         }
-    }
+    };
+    return(
+        <form onSubmit={handleSubmit}>
+            <label>Nombre del cliente</label>
+            <input type="text" value={nombre} onChange={(e) => setName(e.target.value)}/>
+            <br/>
+            <label>Numero de personas</label>
+            <input type="number" value={numeroDePersonas} onChange={(e) => setNumeroDePersonas(e.target.value)}/>
+            <br/>
+            <label>Fecha y hora de la reserva</label>
+            <input type="datetime-local" value={fecha} onChange={(e) => setFecha(e.target.value)}/>
+            <br/>
+            <label>Estado de la reserva</label>
+            <select value={estado} onChange={(e) => setEstado(e.target.value)}>
+                <option value="Confirmada">Confirmada</option>
+                <option value="Cancelada">Cancelada</option>
+            </select>
+            <br/>
+            <button type="Submit">Hacer Reserva</button>
+        </form>
+    )
 }
